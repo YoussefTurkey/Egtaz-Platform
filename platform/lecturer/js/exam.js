00efae14f2fcc9f82c60c33ac4ae84450d4df8ca
Youssef Turkey <youssef.turkey11@gmail.com>
2022-06-28 16:17:21 +0200

    Updating files

--- platform/lecturer/js/exam.js
+++ platform/lecturer/js/exam.js
@@ -2,6 +2,7 @@ const openModalBtn = document.querySelectorAll('[data-modal-target]');
 const closeModalBtn = document.querySelectorAll('[data-close-button]');
 const overlay = document.getElementById('overlay');
 
+
 openModalBtn.forEach(button =>{
     button.addEventListener('click', ()=>{
         const modal =  document.querySelector(button.dataset.modalTarget);
@@ -33,4 +34,60 @@ overlay.addEventListener('click', ()=>{
     modals.forEach(modal => {
         closeModal(modal);
     })
+})
+
+min = 30;
+hour = 1*60;
+let time = setInterval(()=>{
+    timer()
+},1000);
+
+function timer(){
+    min--;
+    if(min==-1){
+        hour--;
+        min=59;
+        if(hour==-1){
+            min=0;
+            hour=0;
+            clearInterval(time);
+            alert('FullTime');
+        }
+    }
+    document.getElementById('min').innerHTML = min;
+    document.getElementById('hour').innerHTML = hour;
+}
+
+const InpBox_1 = document.getElementById('checked_1');
+const ckBox_1 = document.getElementById('ck_1');
+const InpBox_2 = document.getElementById('checked_2');
+const ckBox_2 = document.getElementById('ck_2');
+const InpBox_3 = document.getElementById('checked_3');
+const ckBox_3 = document.getElementById('ck_3');
+const InpBox_4 = document.getElementById('checked_4');
+const ckBox_4 = document.getElementById('ck_4');
+
+const BoolBox_1 = document.getElementById('checked_11');
+const ckBool_1 = document.getElementById('ck_11');
+const BoolBox_2 = document.getElementById('checked_22');
+const ckBool_2 = document.getElementById('ck_22');
+
+InpBox_1.addEventListener('click', ()=>{
+    ckBox_1.click()
+})
+InpBox_2.addEventListener('click', ()=>{
+    ckBox_2.click()
+})
+InpBox_3.addEventListener('click', ()=>{
+    ckBox_3.click()
+})
+InpBox_4.addEventListener('click', ()=>{
+    ckBox_4.click()
+})
+
+BoolBox_1.addEventListener('click', ()=>{
+    ckBool_1.click()
+})
+BoolBox_2.addEventListener('click', ()=>{
+    ckBool_2.click()
 })
